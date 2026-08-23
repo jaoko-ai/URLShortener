d89d4a5440a286d093e35d416ab0b86bdc9696da
Ryan Jaoko <151890018+jaoko-ai@users.noreply.github.com>
2026-08-23 14:12:45 +0300

    Decode and encode

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using URLShortener.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -9,7 +10,37 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+const string ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-app.MapGet("/", () => "Hello World!");
+
+static string Encode(int num)
+{
+    string output = "";
+    if (num == 0)
+    {
+        return "0";
+    }
+    while (num > 0)
+    {
+        output = $"{ALPHABET[num % 62]}{output}";
+        num /= 62;
+    }
+    return output;
+}
+
+
+
+static int Decode(string str)
+{
+    int result = 0;
+    foreach (char character in str)
+    {
+        var value = ALPHABET.IndexOf(character);
+        result = result * 62 + value;
+    }
+    return result;
+}
+
+// if (value === -1) throw new Error('Invalid Base62 character: ' + char);
 
 app.Run();
