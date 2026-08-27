03428f904bc18250dec928b8b6ab30d431a0f0af
Ryan Jaoko <151890018+jaoko-ai@users.noreply.github.com>
2026-08-27 15:57:30 +0300

    Migrations

---
+++ Migrations/20260827125443_SmallFix.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace URLShortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class SmallFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "timestamp",
+                table: "Clicks",
+                newName: "Timestamp");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Urls",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn)
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Timestamp",
+                table: "Clicks",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Clicks",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn)
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clicks_UrlId",
+                table: "Clicks",
+                column: "UrlId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clicks_Urls_UrlId",
+                table: "Clicks",
+                column: "UrlId",
+                principalTable: "Urls",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clicks_Urls_UrlId",
+                table: "Clicks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Clicks_UrlId",
+                table: "Clicks");
+
+            migrationBuilder.RenameColumn(
+                name: "Timestamp",
+                table: "Clicks",
+                newName: "timestamp");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Urls",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "timestamp",
+                table: "Clicks",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Clicks",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn);
+        }
+    }
+}
