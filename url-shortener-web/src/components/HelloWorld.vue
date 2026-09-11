85419fbce75aafa89d7003c509d920d4fc68c4fa
Ryan Jaoko <151890018+jaoko-ai@users.noreply.github.com>
2026-09-11 14:19:56 +0000

    Fix build failures

--- url-shortener-web/src/components/HelloWorld.vue
+++ url-shortener-web/src/components/HelloWorld.vue
@@ -1,36 +1,29 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import axios from "axios";
 
-const MakeGet = async () => {
-	const response = await axios.get("http://localhost:5062/2j", {
-		timeout: 1000,
-	});
-};
-
-let urlInput = ref("");
+const urlInput = ref("");
 
 function PostRequest() {
-	console.error(`User input: ${urlInput.value}`);
-	urlInput.value = "";
+  console.error(`User input: ${urlInput.value}`);
+  urlInput.value = "";
 }
 </script>
 
 <template>
-	<div class="">
-		<div>
-			<Label for="urlShortenerInput"> Enter a URL to shorten: </Label>
-			<input
-				type="text"
-				v-model="urlInput"
-				placeholder="Enter a URL to shorten"
-			/>
+  <div class="">
+    <div>
+      <Label for="urlShortenerInput"> Enter a URL to shorten: </Label>
+      <input
+        type="text"
+        v-model="urlInput"
+        placeholder="Enter a URL to shorten"
+      />
 
-			<button @click="PostRequest">submit</button>
-		</div>
-	</div>
+      <button @click="PostRequest">submit</button>
+    </div>
+  </div>
 
-	<div>
-		<p>Live display: {{ urlInput }}</p>
-	</div>
+  <div>
+    <p>Live display: {{ urlInput }}</p>
+  </div>
 </template>
--- url-shortener-web/src/views/login.vue
+++ url-shortener-web/src/views/login.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
-const message = ref("");
+// const message = ref("");
 const method = ref("login");
 </script>
 <template>
